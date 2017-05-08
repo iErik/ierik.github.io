5534cf91a0ac009c9261efb8617fcc15b6488bf5
Erik Isidore <ericssonico@protonmail.ch>
2017-05-08 02:08:13 -0300

    Implemented About page placeholder components.
    - The About page is split between two components, one for the
    title section and another for the contents of the page. It is
    structure so in order to be used in conjunction with the SplitLayout
    component. The page itself has not been fully implemented yet, these
    are just empty placeholder component modules.

---
+++ src/pages/About/AboutContentSection.vue
@@ -0,0 +1,27 @@
+<script lang="coffee">
+export default AboutContentSection =
+  name: 'about-content-section'
+
+</script>
+
+<template lang="html">
+  <div class="section-content bg-standard">
+
+  </div>
+</template>
+
+<style lang="sass">
+@import ~styles/helpers/_module
+
+$section-foreground-color: foreground-color()
+
+.section-content
+  display: flex
+  flex-direction:  column
+  justify-content: center
+
+  position: absolute
+
+  width:  100%
+  height: 100%
+</style>
