ce27ae360cccf29610b787ae70cb5bb4d670c8bd
Erik Isidore <ericssonico@protonmail.ch>
2017-05-08 02:11:55 -0300

    Implemented Contact page placeholder components
    - The Contact page is split between two components, one for the
    title section and another for the contents of the page. It is
    structure so in order to be used in conjunction with the SplitLayout
    component. The page itself has not been fully implemented yet, these
    are just empty placeholder component files.

---
+++ src/pages/Contact/ContactTitleSection.vue
@@ -0,0 +1,38 @@
+<script lang="coffee">
+export default ContactTitleSection =
+  name: 'contact-title-section'
+
+</script>
+
+<template lang="html">
+  <div class="section-title bg-primary">
+    <header class="wrapper-title">
+      <h1 class="title">Contact</h1>
+    </header>
+  </div>
+</template>
+
+<style lang="sass">
+@import ~styles/helpers/_module
+
+$section-foreground-color: foreground-color()
+
+.section-title
+  display: flex
+  justify-content: center
+
+  width:  100%
+  height: 100%
+
+  transform: scale(1.0) !important
+
+  .wrapper-title
+    margin-top: 25%
+    transform: scale(1.0) !important
+
+    .title
+      font-family: $font-decorative
+      font-size:   4em
+      color:       $section-foreground-color
+
+</style>
