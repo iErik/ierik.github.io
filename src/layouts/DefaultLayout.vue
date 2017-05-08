f097c4e78809656e47b6b8378718cfa6e95c1d57
Erik Isidore <ericssonico@protonmail.ch>
2017-05-08 01:47:14 -0300

    Implemented DefaultLayout Vue component
    - The DefaultLayout component is expected to be used to wrap
    the Homepage for the app.

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,36 @@
+<script lang="coffee">
+import MenuOverlay from 'components/MenuOverlay'
+
+import 'transitions/DefaultLayoutTransition'
+import 'transitions/JumpTransition'
+
+export default DefaultLayout =
+  name: 'default-layout'
+  props:
+    isNavActive:
+      type: Boolean
+      required: yes
+  mounted: ->
+    @$router.app.$emit 'changeLayoutColors', 'accent'
+  components:
+    MenuOverlay: MenuOverlay
+
+</script>
+
+<template lang="html">
+  <default-layout-transition>
+
+    <div class="container">
+      <menu-overlay :is-active="isNavActive"/>
+      <jump-transition>
+        <div class="site-main collapse expanded row" v-show="!isNavActive">
+          <router-view></router-view>
+        </div>
+      </jump-transition>
+    </div>
+
+  </default-layout-transition>
+</template>
+
+<style lang="sass">
+</style>
