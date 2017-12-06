cefc400852d61e2dfe9421c7bce0887aa38c1073
Erik Isidore <ericssonico@protonmail.ch>
2017-12-06 03:21:14 -0200

    Add 404 page
    Implemeted 404 Error page to handle non-existent routes.
    
    Also implemented a helpers module containing a function that will
    filter our top-level routes object and return all page-level routes,
    this might be useful for components that need to be aware of all routes
    that can be navigated directly to (excluding the /404 route, of course)

--- src/config/router.js
+++ src/config/router.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import { filterRoutes } from 'utils/helpers'
+
 import RootLayout from 'layouts/RootLayout'
 import DefaultLayout from 'layouts/DefaultLayout'
 import SplitLayout from 'layouts/SplitLayout'
 
 import Homepage from 'pages/Homepage'
+import PageNotFound from 'pages/404'
 
 import * as Projects from 'pages/Projects'
 import * as Contact from 'pages/Contact'
@@ -17,11 +20,19 @@ export const routes = [
   { path: ''
   , abstract: true
   , component: DefaultLayout
+  , type: 'layout'
   , children:
   [
     { path: '/'
     , name: 'homepage'
+    , type: 'page'
     , component: Homepage
+    },
+
+    { path: '/404'
+    , name: 'page-not-found'
+    , type: 'error-page'
+    , component: PageNotFound
     }
   ]},
 
@@ -29,10 +40,12 @@ export const routes = [
   , component: SplitLayout
   , name: 'split-layout'
   , abstract: true
+  , type: 'layout'
   , children:
   [
     { path: '/about'
     , name: 'about'
+    , type: 'page'
     , components:
       { leftColumn: About.TitleSection
       , rightColumn: About.ContentSection
@@ -41,6 +54,7 @@ export const routes = [
 
     { path: '/projects/:projectName?'
     , name: 'projects'
+    , type: 'page'
     , components:
       { leftColumn:  Projects.TitleSection
       , rightColumn: Projects.ContentSection
@@ -49,6 +63,7 @@ export const routes = [
 
     { path: '/contact'
     , name: 'contact'
+    , type: 'page'
     , components:
       { leftColumn: Contact.TitleSection
       , rightColumn: Contact.ContentSection
@@ -62,4 +77,12 @@ const router = new VueRouter({
   routes,
 });
 
+router.beforeEach((to, from, next) => {
+  if (!to.matched.length)
+    next('/404');
+  else
+    next();
+});
+
+export const pageRoutes = filterRoutes(routes);
 export default router;
